0cd1ba21b43b7a47c29f5df757140d7e4e08e34b
KUDPC-USER <a0147155@laurel1.kudpc.kyoto-u.ac.jp>
2018-06-24 18:51:49 +0900

    hoge

---
+++ myomp2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<omp.h>
+
+#define L 1000LL
+#define R 2000000LL
+
+int main(){
+  long long n, i, j, t;
+  long long answer;
+  double t1, t2;
+
+  t1 = omp_get_wtime();
+
+  answer = 0;
+#pragma omp parallel
+{ 
+ #pragma omp for private(j,t) reduction(+:answer)
+  for(i=L; i<=R; i++){
+
+  t = i;
+  for(j=2; j*j<=i; j++){
+     if(i%j==0){
+        t = j;
+        break;
+      }
+    }
+    answer += t;
+  }
+
+}
+
+  t2 = omp_get_wtime();
+
+  printf("answer %lld\n", answer);
+  printf("time %f\n", t2-t1);
+  
+  return 0;
+}
